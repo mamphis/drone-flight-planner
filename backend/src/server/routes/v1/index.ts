2a356aa02b01fd5ce77fadb9f49d9ae2bd030435
mamphis <mattis.wacker@pcsmw.de>
2022-06-29 22:47:27 +0200

    feature(backend): Added mission router
    Added the get action for the mission router

--- backend/src/server/routes/v1/index.ts
+++ backend/src/server/routes/v1/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from './user';
 import teamRouter from './team';
+import missionRouter from './mission';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/teams', teamRouter);
+router.use('/missions', missionRouter);
 
 export default router;
---
+++ backend/src/server/routes/v1/mission.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client';
+import { Router, Request, Response, NextFunction } from 'express';
+import authHandler from '../../middleware/auth';
+
+const router = Router();
+const client = new PrismaClient();
+
+router.use(authHandler);
+
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const missions = await client.flightMission.findMany({
+            where: {
+                team: {
+                    OR: [
+                        {
+                            members: {
+                                some: {
+                                    id: res.locals.user.id,
+                                }
+                            }
+                        },
+                        {
+                            owner: {
+                                id: res.locals.user.id,
+                            },
+                        },
+                    ]
+                },
+            },
+            select: {
+                id: true,
+                name: true,
+                team: {
+                    select: {
+                        id: true,
+                        name: true,
+                        owner: {
+                            select: {
+                                id: true,
+                                name: true,
+                                username: true,
+                            },
+                        },
+                    },
+                },
+                flightAltitude: true,
+                flightSpeed: true,
+                homeLatitude: true,
+                homeLongitude: true,
+            },
+        });
+
+        res.json(missions);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+export default router;
