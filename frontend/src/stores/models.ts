c117ba2accf4391411f2acd1a0af8d0fb7e72820
mamphis <mattis.wacker@pcsmw.de>
2022-07-09 10:21:40 +0200

    feature(frontend): Added Missions view and load missions from backend

---
+++ frontend/src/stores/models.ts
@@ -0,0 +1,30 @@
+export type User = {
+    username: string,
+    email: string,
+    id: string,
+    name: string
+};
+
+export type TeamLean = {
+    id: string;
+    name: string;
+    owner: User;
+}
+
+export type TeamOverview = TeamLean & {
+    _count: {
+        members: number;
+    };
+    createdAt: string;
+    updatedAt: string;
+};
+
+export type FlightMission = {
+    id: string;
+    name: string;
+    team: TeamLean;
+    flightAltitude: number;
+    flightSpeed: number;
+    homeLatitude: number;
+    homeLongitude: number;
+}
