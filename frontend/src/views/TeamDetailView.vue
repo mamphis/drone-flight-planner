3e580d690c3238d24c0ffc98d9c44c2699199d45
mamphis <mattis.wacker@pcsmw.de>
2022-08-04 18:35:24 +0200

    feature(frontend): Added Flights to TeamDetails

--- frontend/src/views/TeamDetailView.vue
+++ frontend/src/views/TeamDetailView.vue
@@ -1,15 +1,24 @@
 <script lang="ts" setup>
+import { Translator } from '@/libs/localization/localizator.js';
 import { teamStore } from '@/stores/team';
 import { userStore } from '@/stores/user';
+import { inject, ref } from 'vue';
 import { useRouter } from 'vue-router';
+
+const $l = inject<Translator>('$l')!;
 
 const router = useRouter();
 const id = router.currentRoute.value.params.id as string;
 
 const teamDb = teamStore();
 const userDb = userStore();
-const team = await teamDb.getTeam(id);
-const editable = team.owner.id === userDb.user?.id;
+const team = ref(await teamDb.getTeam(id));
+const editable = team.value.owner.id === userDb.user?.id;
+
+async function removeMember(memberId: string) {
+    await teamDb.removeMember(id, memberId);
+    team.value = await teamDb.getTeam(id);
+}
 </script>
 
 
@@ -18,17 +27,53 @@ const editable = team.owner.id === userDb.user?.id;
         <div class="team">
             <div class="team-name">
                 <h1>{{ team.name }}</h1>
+                <sub>Owner: {{ team.owner.name }}</sub>
             </div>
-            <div class="team-members">
-                <div class="team-member" v-for="member in team.members" :key="member.id">
-                    <div class="team-member-name">
+            <div class="list-container">
+                <h2 class="header" v-text="$l('team.labels.members')" />
+                <div class="row" v-for="member in team.members" :key="member.id">
+                    <div class="col">
                         {{ member.name }}
                     </div>
-                    <div class="team-member-email">
+                    <div class="col">
                         {{ member.email }}
+                    </div>
+                    <div v-if="editable" class="actions">
+                        <button @click="removeMember(member.id)"><i class="fas fa-trash" /> {{
+                                $l('teams.labels.removeMember')
+                        }}</button>
+                    </div>
+                </div>
+            </div>
+            <div class="list-container clickable">
+                <h2 class="header" v-text="$l('team.labels.flights')" />
+                <div class="row" v-for="flight in team.flightMissions" :key="flight.id"
+                    @click.stop="router.push({ path: `/flights/${flight.id}` })">
+                    <div class="col">
+                        {{ flight.name }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<style scoped>
+@import "@/assets/lists.css";
+
+.container,
+.team {
+    min-height: 100%;
+    position: absolute;
+    width: 100%;
+}
+
+.team {
+    display: flex;
+    flex-direction: column;
+}
+
+.team .list-container {
+    flex-grow: 1;
+    flex-basis: 100%;
+}
+</style>
